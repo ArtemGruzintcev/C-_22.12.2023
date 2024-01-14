ee5f675982976945381fb4bd2b44c3bc73172cab
Артем <artem_frolov346@mail.ru>
2024-01-14 20:55:56 +0700

    Lesson 2 task 4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,26 @@
+﻿//* Напишите программу, которая выводит третью с конца
+//* цифру заданного числа или сообщает, что третьей цифры
+//* нет.
+//? 456 => 6
+//? 7812 => 8
+//? 91 => Третьей цифры нет
+
+// Console.WriteLine("Введите число");
+// long num = Convert.ToInt32(Console.ReadLine());
+
+// if (num < 100)
+// {
+//     Console.WriteLine("третьей цыфры нет");
+// }
+
+// if (num >= 1000)
+// {
+//     long num1 = (num / 100) % 10;
+//     Console.WriteLine(num1);
+// }
+
+// else if (num >= 100 && num < 1000)
+// {
+//     long num2 = num % 10;
+//     Console.WriteLine(num2);
+// }
