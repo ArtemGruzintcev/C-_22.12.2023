e2fd37f52068dce386b10e1b8643be8bb0e67d53
Артем <artem_frolov346@mail.ru>
2024-01-14 20:45:18 +0700

    homework bumber 1

---
+++ Lesson_1/HW_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит,
+// какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите число");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber > secondNumber)
+// Console.WriteLine( $"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+// if (firstNumber == secondNumber)
+// {
+//     Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+// }
