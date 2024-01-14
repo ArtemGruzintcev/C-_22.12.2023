a185f51671fe63bb511c200bbb7b3a2c2852d41a
Артем <artem_frolov346@mail.ru>
2024-01-14 20:12:41 +0700

    Lesson 1 task 1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿//* 1 Задача.
+//* Напишите программу, которая на вход
+//* принимает два целых числа и проверяет,
+//* является ли первое число квадратом второго.
+//? Примеры:
+//? a = 25, b = 5 => да
+//? a = 2, b = 10 => нет
+//? a = 9, b = -3 => да
+//? a = -3, b = 9 => нет
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 %num2==0)
+// {
+//     Console.WriteLine("да");
+// }
+// else
+// {
+//     Console.WriteLine($"нет, остаток {num1 %num2}");
+// }
