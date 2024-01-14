290be5948032aa7db09c2364e59d16d2119cedf7
Артем <artem_frolov346@mail.ru>
2024-01-14 20:47:19 +0700

    homework bumber 2

---
+++ Lesson_1/HW_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Внутри класса Answer напишите метод FindMax,
+// который принимает на вход три числа и выдаёт максимальное из этих чисел
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b && a > c)
+// {
+//     Console.WriteLine(a);
+// }
+// else
+// {
+//     if (b > a && b > c)
+//     {
+//         Console.WriteLine(b);
+//     }
+//     else
+//     {
+//         if (c > a && c > b)
+//         {
+//             Console.WriteLine(c);
+//         }
+//     }
+// }
