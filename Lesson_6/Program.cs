e9baf21049633880690bbc8510af5136fbd28dbc
Артем <artem_frolov346@mail.ru>
2024-01-29 02:18:13 +0700

    Lesson 6

---
+++ Lesson_6/Program.cs
@@ -0,0 +1,61 @@
+﻿//* Задайте массив символов (тип char []). Создайте строку из
+//* символов этого массива.
+//! Указание
+//! Конструктор строки вида string(char []) не использовать.
+//? Пример
+//? [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+// class Program
+// {
+//     static void Main(string [] args)
+//     {
+//         Console.Clear();
+//         char[] chars = new char[] {'a','b','c','d'};
+//         for (int y = 0; y < chars.Length; y++)
+//         {
+//             Console.Write(chars[y]);
+//         }
+//         Console.ReadLine();
+//     }
+// }
+
+//* На основе символов строки (тип string) сформировать массив
+//* символов (тип char[]). Вывести массив на экран.
+//? Пример
+//? “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’] 
+
+// class Program
+// {
+//     static void Main(string [] args)
+//     {
+//         Console.Clear();
+//         string Hello = "Hello!";
+//         char [] chars = new char[Hello.Length];
+//         for (int y = 0; y < Hello.Length; y++)
+//         {
+//             chars[y] = Hello[y];
+//             Console.Write(chars[y] + "\t");
+//         }
+//         Console.ReadLine();
+//     }
+// }
+
+//* Считать строку с консоли, состоящую из латинских
+//* букв в нижнем регистре. Выяснить, сколько среди
+//* введённых букв гласных.
+//? Пример
+//? “hello” => 2
+//? “world” => 1
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+    //     Console.WriteLine("Введите слово: ");
+    //     string word = Console.ReadLine();
+    //     shar[] Word = new char[word.Length]
+    //     System.Console.WriteLine(word);
+    
+    }
+}
