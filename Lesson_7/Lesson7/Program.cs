220e53f0063a81b7cf13dd28161a5ea473ac23e8
Артем <artem_frolov346@mail.ru>
2024-02-01 22:41:46 +0700

    Lesson 7

---
+++ Lesson_7/Lesson7/Program.cs
@@ -0,0 +1,25 @@
+﻿//* Напишите программу, которая будет принимать на вход число и
+//* возвращать сумму его цифр.
+//! Указание
+//! Использовать рекурсию.
+//? Пример:
+//? 123 => 6
+//? 63 => 9
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine(Sum(num));
+    }
+    static int Sum(int num)
+    {
+        if (num > 10) // проверяем чтобы число было не меньше 10 
+            return num;
+
+        return num % 10 + Sum(num / 10);
+    }
+}
